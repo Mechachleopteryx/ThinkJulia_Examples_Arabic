879ed2bb301f3530758eb156bbc4bebaa9617593
Moulay Driss EL ALAOUI FARIS <elalaouifaris@gmail.com>
2020-08-16 11:05:01 +0200

    First 4 notebooks with video links per sections

--- utils.jl
+++ utils.jl
@@ -1,8 +1,17 @@
 module Utils
 
-export videoHTML
+export read_youtube
 
-function videoHTML(video_id, start_sec, end_sec)
+function read_youtube(video_id, end_time::String)
+    read_youtube(video_id, "1", end_time)
+end
+
+function read_youtube(video_id, start_time::String, end_time::String)
+    start_sec, end_sec = parse_time(start_time), parse_time(end_time)
+    videoHTML(video_id, start_sec, end_sec)
+end
+
+function videoHTML(video_id, start_sec::Integer, end_sec::Integer)
     HTML("""
         <iframe width="560" height="315"
         src="https://www.youtube.com/embed/$video_id?start=$start_sec&end=$end_sec&rel=0&showinfo=0"
@@ -10,4 +19,13 @@ function videoHTML(video_id, start_sec, end_sec)
         </iframe>
     """)
 end
+
+function parse_time(str_time)
+    parsed_time = map(x -> parse(Int64, x), split(str_time, ":"))
+    count_seconds(parsed_time...)
+end
+
+count_seconds(s) = s
+count_seconds(m, s) = 60m + s
+count_seconds(h, m, s) = count_seconds(60h + m, s)
 end
